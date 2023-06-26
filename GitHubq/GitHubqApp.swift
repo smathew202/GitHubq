3a469acf22a4a2d598582255752b8aeddeead0e0
Simonni Mathew <simalexis0105@gmail.com>
2023-06-26 15:29:18 -0400

    Initial Commit

---
+++ GitHubq/GitHubqApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubqApp.swift
+//  GitHubq
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
